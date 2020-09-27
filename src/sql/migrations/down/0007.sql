39398000688b727109dbb026d853fa15d33e9499
Hypolite Petovan <hypolite@mrpetovan.com>
2020-09-27 08:39:18 -0400

    [Database v0008b] Add missing column in fulltext index
    - Add minimal SQL query to downgrade SQL script to avoid empty query errors

--- src/sql/migrations/down/0007.sql
+++ src/sql/migrations/down/0007.sql
@@ -1,4 +1,4 @@
 BEGIN;
 ALTER TABLE `profile` DROP KEY `profile-ft`;
-ALTER TABLE `profile` ADD FULLTEXT KEY `profile-ft` (`name`, `pdesc`, `profile_url`, `locality`, `region`, `country`);
+ALTER TABLE `profile` ADD FULLTEXT KEY `profile-ft` (`name`, `pdesc`, `profile_url`, `locality`, `region`, `country`, `tags`);
 COMMIT;
--- src/sql/migrations/up/0008.sql
+++ src/sql/migrations/up/0008.sql
@@ -1,4 +1,4 @@
 BEGIN;
 ALTER TABLE `profile` DROP KEY `profile-ft`;
-ALTER TABLE `profile` ADD FULLTEXT KEY `profile-ft` (`name`, `pdesc`, `username`, `locality`, `region`, `country`);
+ALTER TABLE `profile` ADD FULLTEXT KEY `profile-ft` (`name`, `pdesc`, `username`, `locality`, `region`, `country`, `tags`);
 COMMIT;
